5eeea21593584cb01a76071743d733dc8b943f5c
Nathaniel Patterson <odst0016@gmail.com>
2021-05-31 19:26:32 -0400

    added user login and a schema for users

---
+++ api/users/models/userSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  telephone: {
+    type: String,
+    required: true,
+  },
+  ownedPlants: [],
+});
+
+module.exports = mongoose.model("users", userSchema);
--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -1,28 +1,38 @@
 const router = require("express").Router();
+const User = require("./models/userSchema");
+const bcryptjs = require("bcryptjs");
 
 //Gets plants accessable to user. Requires user to be logged in.
-router.get("/plants", (req, res) =>{
-
-})
+router.get("/plants", (req, res) => {});
 
 //Deletes a user and all plants associated with them. Requires user to be logged in.
-router.delete("/", (req, res)=>{
-
-})
+router.delete("/", (req, res) => {});
 
 //Registers a new user.
-router.post("/", (req, res) =>{
+router.post("/register", async (req, res) => {
+  const credentials = req.body;
 
-})
+  const hash = bcryptjs.hashSync(credentials.password, 10);
+  credentials.password = hash;
+
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    telephone: req.body.telephone,
+  });
+
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //Logs a user in.
-router.post("/" (req, res) =>{
-
-})
+router.post("/login", (req, res) => {});
 
 //Changes a users password. Requires a user to be logged in and pass password validation checks.
-router.patch("/password", (req, res) =>{
-
-})
+router.patch("/password", (req, res) => {});
 
 module.exports = router;
